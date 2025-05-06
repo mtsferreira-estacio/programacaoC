0c8c1635bbcd2255fc8d44d0de64494f53d2728e
Mateus Ferreira <mateusn.ferreira8@gmail.com>
2025-05-06 15:24:47 +0000

    Changed position and add a comment

--- supertrunfo.c
+++ supertrunfo.c
@@ -6,6 +6,7 @@ int main() {
     float area1, pib1, area2, pib2;
     char estado1, codEstado1[3], nomeCidade1[50], estado2, codEstado2[3], nomeCidade2[50];
 
+    // Cadastro das Cartas:
     // Entrada de dados Carta 1:
     printf("        Digite os dados da carta 1:\n");
     printf("===========================================\n");
@@ -25,23 +26,10 @@ int main() {
     scanf("%d", &pontosTuristicos1);
 
     printf("PIB: ");
-    scanf("%f", &pib1);
+    scanf("%f", &pib1); // Não há tratamento algum, então é o valor que o usuário digitar.
 
     printf("Área(km²): ");
     scanf("%f", &area1);
-
-    // Output da Carta 1:
-    printf("===========================================\n");
-    printf("                Carta 1:                \n");
-    printf("Estado: %c\n", estado1);
-    printf("Código do Estado: %s\n", codEstado1);
-    printf("Código da Carta: %c%s\n", estado1,codEstado1);
-    printf("Cidade: %s\n", nomeCidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.2fkm²\n", area1);
-    printf("PIB: %.2fR$\n", pib1);
-    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
-    printf("===========================================\n");
 
     // Entrada de dados Carta 2:
     printf("        Digite os dados da carta 2:\n");
@@ -62,10 +50,24 @@ int main() {
     scanf("%d", &pontosTuristicos2);
 
     printf("PIB: ");
-    scanf("%f", &pib2);
+    scanf("%f", &pib2); // Não há tratamento algum, então é o valor que o usuário digitar.
 
     printf("Área(km²): ");
     scanf("%f", &area2);
+
+    // Exibição dos Dados das Cartas:
+    // Output da Carta 1:
+    printf("===========================================\n");
+    printf("                Carta 1:                \n");
+    printf("Estado: %c\n", estado1);
+    printf("Código do Estado: %s\n", codEstado1);
+    printf("Código da Carta: %c%s\n", estado1,codEstado1);
+    printf("Cidade: %s\n", nomeCidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2fkm²\n", area1);
+    printf("PIB: %.2fR$\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("===========================================\n");
 
     // Output da Carta 2:
     printf("===========================================\n");
